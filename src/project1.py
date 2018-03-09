277a3ae7aa6aebabc4d8c00293b6f36b95f3e12a
Bilal <bshehata@student.unimelb.edu.au>
2018-03-10 10:50:34 +1100

    preprocessing done

--- src/project1.py
+++ src/project1.py
@@ -1,9 +1,17 @@
+import numpy as np  
+import pandas as pd
+import csv
 # This function should open a data file in csv, and transform it into a usable format 
 def preprocess():
-    return
+    filepath = "../datasets/car-dos.csv"
+  
+
+    df = pd.read_csv(filepath, header = None)
+    return df
 
 # This function should build a supervised NB model
 def train_supervised():
+    print(df.groupby(len(df.columns)-1).size().div(len(df)))
     return
 
 # This function should predict the class for a set of instances, based on a trained model 
@@ -25,4 +33,6 @@ def predict_unsupervised():
 # This function should evaluate a set of predictions, in an unsupervised manner
 def evaluate_unsupervised():
     return
+
+preprocess()
 
