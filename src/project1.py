a92302d826d0096fe5b00cdf2425c8391aadc278
OpticGenius <opticgenius@hotmail.com>
2018-03-09 16:21:11 +1100

    Added datasets and boilterplate code

---
+++ src/project1.py
@@ -0,0 +1,28 @@
+# This function should open a data file in csv, and transform it into a usable format 
+def preprocess():
+    return
+
+# This function should build a supervised NB model
+def train_supervised():
+    return
+
+# This function should predict the class for a set of instances, based on a trained model 
+def predict_supervised():
+    return
+
+# This function should evaluate a set of predictions, in a supervised context 
+def evaluate_supervised():
+    return
+
+# This function should build an unsupervised NB model 
+def train_unsupervised():
+    return
+
+# This function should predict the class distribution for a set of instances, based on a trained model
+def predict_unsupervised():
+    return
+
+# This function should evaluate a set of predictions, in an unsupervised manner
+def evaluate_unsupervised():
+    return
+
