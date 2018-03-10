164b6397dc1111eb45cd811a2de503717705b2db
Bilal <bshehata@student.unimelb.edu.au>
2018-03-10 16:07:45 +1100

    comments

--- src/project1.py
+++ src/project1.py
@@ -4,7 +4,7 @@ import csv
 from collections import defaultdict
 # This function should open a data file in csv, and transform it into a usable format 
 def preprocess():
-    filepath = "../datasets/cars-train.csv"
+    filepath = "../datasets/car-dos.csv"
 
 
     df = pd.read_csv(filepath, header = None)
@@ -12,21 +12,22 @@ def preprocess():
 
 # This function should build a supervised NB model
 def train_supervised():
+    #prior probabilties
     highProb = df.groupby(len(df.columns)-1).size().div(len(df))
      
-    print(highProb)
+    #probabiltiy deictionary 
     probList = defaultdict(lambda: defaultdict(float))
     
-    
+    #this loop calculates the probabilties 
     for i in range(0,len(df.columns)-1):
         cols =[len(df.columns)-1,i]
         trained = df.groupby(cols).size().div(len(df)).div(highProb, axis = 0,level=(len(df.columns)-1))
         probList[i] = trained
     
-  
+    #converts dataframe into dictionary 
     for i in range(0,len(probList)):
         probList[i] = probList[i].to_dict()
-
+        #collects the classes into an list
     classes = df[len(df.columns)-1].unique()
     return probList, classes
 
@@ -34,27 +35,27 @@ def train_supervised():
 def predict_supervised(probList,testrow):
     
     
-
+    
   
-    classChance = list()
+    classChance = list() #keep record of all classchanes for this row
     for possibleClass in classes : 
-        prob =1 
-        for i in range(0,len(testrow)) :
+        prob =1  #begin at 1 
+        for i in range(0,len(testrow)) : #for every element multiply in 
         
             if (possibleClass,testrow[i]) in probList[i]: 
                 prob = prob * probList[i][(possibleClass,testrow[i])]
             else : 
-                prob=prob * 0.00001
-        classChance.append(prob)
+                prob=prob * 0.0000001 #epsilon 
+        classChance.append(prob) #add the item to the list
   
-  
+            #here we get the highest probability and match it to the class
     return classes[classChance.index(max(classChance))]
 
 # This function should evaluate a set of predictions, in a supervised context 
 def evaluate_supervised(testcsv):
     correct = 0
     total = 0 
-    
+    #iterate over each of the rows and pass it to the predict supervised method
     for index, testrow in testcsv.iterrows() :
         print(testrow.tolist())
         if predict_supervised(probs, testrow.tolist()) == testrow[len(testcsv.columns)-1]:
@@ -77,10 +78,13 @@ def predict_unsupervised():
 def evaluate_unsupervised():
     return
 
+#load in test data
 testdata = "../datasets/cars-test.csv"
 testcsv = pd.read_csv(testdata, header = None)
-
+#preprocess the data
 df = preprocess()
+#train the data
 probs, classes = train_supervised()
+#evaluate the data 
 evaluate_supervised(testcsv)
 
