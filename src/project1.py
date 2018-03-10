87b28b8478dfa6861205c951eecc6c06f8f2d4b0
Bilal <bshehata@student.unimelb.edu.au>
2018-03-10 14:50:06 +1100

    first unsupervised trained

--- src/project1.py
+++ src/project1.py
@@ -1,9 +1,10 @@
 import numpy as np  
 import pandas as pd
 import csv
+from collections import defaultdict
 # This function should open a data file in csv, and transform it into a usable format 
 def preprocess():
-    filepath = "../datasets/breast-cancer-dos.csv"
+    filepath = "../datasets/car-dos.csv"
   
 
     df = pd.read_csv(filepath, header = None)
@@ -14,18 +15,34 @@ def train_supervised():
     highProb = df.groupby(len(df.columns)-1).size().div(len(df))
     
     print(highProb)
+    probList = defaultdict(lambda: defaultdict(float))
     
-    cols =list(range(0,len(df.columns)))
-    print(cols)
+    for i in range(0,len(df.columns)-1):
+        cols =[len(df.columns)-1,i]
+        trained = df.groupby(cols).size().div(len(df)).div(highProb, axis = 0,level=(len(df.columns)-1))
+        probList[i] = trained
+    
+    print(probList)
+    for i in range(0,len(probList)):
+        probList[i] = probList[i].to_dict()
+    print(probList)
   
-    trained = df.groupby(cols).size().div(len(df)).div(highProb, axis = 0,level=(len(df.columns)-1))
-    print(trained)
-  
-    return
+    return probList
 
 # This function should predict the class for a set of instances, based on a trained model 
-def predict_supervised():
+def predict_supervised(probList):
     
+    
+    testrow = ['low','low','2','4','big','med','good']
+    prob = 1
+    for i in range(0,len(testrow)) :
+        
+        if ('acc',testrow[i]) in probList[i]: 
+            prob = prob * probList[i][('acc',testrow[i])]
+        else : 
+            prob=prob * 0.00001
+  
+    print(prob)
     return
 
 # This function should evaluate a set of predictions, in a supervised context 
@@ -45,5 +62,6 @@ def evaluate_unsupervised():
     return
 
 df  = preprocess()
-train_supervised()
+probs = train_supervised()
+predict_supervised(probs)
 
