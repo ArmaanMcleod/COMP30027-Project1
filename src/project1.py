986e9d911b834f1583b22bf05458208c94a22a67
OpticGenius <opticgenius@hotmail.com>
2018-03-10 17:52:21 +1100

    Starting comments

--- src/project1.py
+++ src/project1.py
@@ -4,9 +4,17 @@ from pprint import pprint
 
 # This function should open a data file in csv, and transform it into a usable format 
 def preprocess(filename):
+
+    # all lines in file go here
     lines = []
+
+    # open file
     with open(filename, 'r') as infile:
+
+        # convert to reader object
         reader = csv.reader(infile)
+
+        # loop over each line and add it to resultant list
         for line in reader:
             lines.append(line)
 
@@ -14,40 +22,34 @@ def preprocess(filename):
 
 # This function should build a supervised NB model
 def train_supervised(training_data):
-    class_counts = defaultdict(int)
 
+    # posterier probablities
+    priors = defaultdict(int)
+
+    # number of lines in preprocessed training data
     num_lines = len(training_data)
 
+    # count prior probabilities
     for line in training_data:
         col = line[-1]
-        class_counts[col] += 1
+        priors[col] += 1
 
-    priors = {k: v / num_lines for k, v in class_counts.items()}
+    priors = {k: v / num_lines for k, v in priors.items()}
 
-    #print(priors)
-
-    counts = defaultdict(lambda : defaultdict(lambda : defaultdict(int)))
+    posterier = defaultdict(lambda : defaultdict(lambda : defaultdict(int)))
     for line in training_data:
         attributes = line[:-1]
         class_name = line[-1]
         for attribute, value in enumerate(attributes):
-            counts[class_name][attribute][value] += 1
+            posterier[class_name][attribute][value] += 1
  
-    for class_name in counts:
-        for attribute in counts[class_name]:
-            sums = sum(counts[class_name][attribute].values())
-            for count in counts[class_name][attribute]:
-                counts[class_name][attribute][count] /= sums
-                
-    pprint(counts)
+    for class_name in posterier:
+        for attribute in posterier[class_name]:
+            sums = sum(posterier[class_name][attribute].values())
+            for count in posterier[class_name][attribute]:
+                posterier[class_name][attribute][count] /= sums
 
-
-            
-
-
-
-
-
+    return priors, posterier
 
 # This function should predict the class for a set of instances, based on a trained model 
 def predict_supervised():
@@ -73,7 +75,7 @@ if __name__ == '__main__':
     dataset = 'C:\\Users\\Alex\\Documents\\mygit\\COMP30027-Project1\\datasets\\car-dos.csv'
     data = preprocess(dataset)
 
-    print(data)
+    #print(data)
 
     print(train_supervised(data))
 
