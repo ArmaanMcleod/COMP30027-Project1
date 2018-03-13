3cbfadebc381dbae4800a7e9a63ac47c04b671e5
OpticGenius <opticgenius@hotmail.com>
2018-03-13 21:22:24 +1100

    Created the supervised predication function

--- project1.py
+++ project1.py
@@ -1,6 +1,7 @@
 import csv
-from collections import defaultdict
 from pprint import pprint
+from collections import defaultdict
+from operator import itemgetter
 
 # This function should open a data file in csv, and transform it into a usable format 
 def preprocess(filename):
@@ -23,7 +24,7 @@ def preprocess(filename):
 # This function should build a supervised NB model
 def train_supervised(training_data):
 
-    # posterier probablities
+    # posteriers probablities
     priors = defaultdict(int)
 
     # number of lines in preprocessed training data
@@ -37,10 +38,11 @@ def train_supervised(training_data):
     # update dictionary with probabilites instead of counts
     priors = {k: v / num_lines for k, v in priors.items()}
 
-    # create posterier data structure with triple nested defaultdicts
+    # create posteriers data structure with triple nested defaultdicts
     # perhaps a more simplified data structure could be used here
     # initialising int here initially for storing frequencies
-    posterier = defaultdict(lambda : defaultdict(lambda : defaultdict(int)))
+    # initialise frequency to 1 for smoothing
+    posteriers = defaultdict(lambda : defaultdict(lambda : defaultdict(int)))
 
     # Loop over each line in preprocessed training data
     for line in training_data:
@@ -51,28 +53,60 @@ def train_supervised(training_data):
 
         # count each item
         # using indices as attribute headers since not given in dataset
+        # indice starts at one
         # perhaps datasets could be modified to include header names
         for attribute, freq in enumerate(attributes):
-            posterier[class_name][attribute][freq] += 1
+            posteriers[class_name][attribute][freq] += 1
  
-    # transform posterier counts into probabilities
-    for class_name in posterier:
-        for attribute in posterier[class_name]:
+    # transform posteriers counts into probabilities
+    for class_name in posteriers:
+        for attribute in posteriers[class_name]:
 
             # sum counts over each dict
             # This will be the same accross each class
-            sums = sum(posterier[class_name][attribute].values())
+            sums = sum(posteriers[class_name][attribute].values())
 
             # update to probabilites by dividing freq/sums
-            for freq in posterier[class_name][attribute]:
-                posterier[class_name][attribute][freq] /= sums
+            for freq in posteriers[class_name][attribute]:
+                posteriers[class_name][attribute][freq] /= sums
 
     # return a tuple of the two above data structures
-    return priors, posterier
+    return priors, posteriers
 
 # This function should predict the class for a set of instances, based on a trained model 
-def predict_supervised():
-    return
+def predict_supervised(priors, posteriers, instance):
+
+    # epsilon value for smoothing
+    EPSILON = 0.000000000001
+
+    # dictionary holding maximal for each class
+    class_max = {}
+
+    # go through each class
+    for class_name in posteriers:
+
+        # product accumulator
+        product = 1
+
+        # go over attributes of the trained data and instances at once
+        for attribute, value in zip(posteriers[class_name], instance):
+
+            # get the hashed probability
+            loc = posteriers[class_name][attribute][value]
+
+            # if probability is non-zero, accumulate it
+            if loc:
+                product *= loc
+
+            # otherwise, accumulate the smoothing factor
+            else:
+                product *= EPSILON
+
+        # add accumulated values to class maximals
+        class_max[class_name] = product * priors[class_name]
+
+    # return a tuple of the class and maximal probability
+    return max(class_max.items(), key=itemgetter(1))
 
 # This function should evaluate a set of predictions, in a supervised context 
 def evaluate_supervised():
@@ -96,10 +130,21 @@ def main():
                 'hypothyroid-dos.csv',
                 'mushroom-dos.csv']
 
-    for file in datasets:
-        data = preprocess(file)
-        trained_data = train_supervised(data)
-        print(trained_data)
+    #for file in datasets:
+        #data = preprocess(file)
+        #trained_data = train_supervised(data)
+        #print(trained_data)
+
+    # testing on just one dataset
+    data = preprocess(datasets[1])
+    priors, posteriers = train_supervised(data)
+
+    # test row here
+    instance = ['vhigh','vhigh', '2', '4', 'small', 'med', 'unnac']
+    print(predict_supervised(priors, posteriers, instance[:-1]))
+
+
+    #print(trained_data)
 
 
 if __name__ == '__main__':
